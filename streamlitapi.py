db8b56756c246f377897a5994427c040efa11e21
wanwhasonia <wanwha.sonia.putri-2019@fst.unair.ac.id>
2022-12-13 19:26:09 -0800

    upload streamlitapi

---
+++ streamlitapi.py
@@ -0,0 +1,26 @@
+import pickle
+import streamlit as st
+
+model= pickle.load(open('svm.pkl','rb'))
+
+def main():
+    st.title('Prediksi Kejadian Hujan Esok Hari')
+
+    #input variables
+    Rainfall = st.text_input('Rainfall : ')
+    Sunshine = st.text_input('Sunshine : ')
+    WindGustSpeed = st.text_input('WindGustSpeed')
+    Humidity3pm = st.text_input('Humidity3pm')
+    Pressure9am = st.text_input('Pressure9am')
+    RainToday = st.text_input('RainToday')
+    
+    #prediction code
+    if st.button('Predict'):
+        makeprediction = model.predict([[Rainfall, Sunshine, WindGustSpeed, Humidity3pm, Pressure9am, RainToday]])
+        output=makeprediction[0]
+        st.success('Besok tidak turun hujan')
+        output=makeprediction[1]
+        st.success('Besok akan turun hujan')
+
+if __name__=='__main__':
+    main()
